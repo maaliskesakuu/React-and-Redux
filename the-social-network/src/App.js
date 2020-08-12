40e66d4affae082b3a0b5fb753738b8d44d91b8c
maaliskesakuu <tanja@prodi.fi>
2020-08-12 10:42:08 +0300

    added Firebase

--- the-social-network/src/App.js
+++ the-social-network/src/App.js
@@ -8,6 +8,40 @@ import LogOut from "./Components/Auth/LogOut";
 import Feeds from "./Components/HomePage/Feeds";
 import CreateNewPost from "./Components/Posts/CreateNewPost";
 import PostDetails from "./Components/Posts/PostDetails";
+import Firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAUsEs1fxdrTLc1hjVaEDn5HZCi45Cqt6c",
+  authDomain: "social-network-b2d25.firebaseapp.com",
+  databaseURL: "https://social-network-b2d25.firebaseio.com",
+  projectId: "social-network-b2d25",
+  storageBucket: "social-network-b2d25.appspot.com",
+  messagingSenderId: "354221347923",
+  appId: "1:354221347923:web:a2a750235710a56c63e2a6",
+  measurementId: "G-T75GK27XKD",
+};
+// Initialize Firebase
+Firebase.initializeApp(firebaseConfig);
+Firebase.analytics();
+
+const database = Firebase.firestore();
+
+database
+  .collection("posts")
+  .get()
+  .then(resp => {
+    console.log("resp is: ");
+    console.log(resp);
+    console.log('resp.docs is: ');
+    console.log(resp.docs);
+    console.log('resp.docs[0].data()');
+    console.log(resp.docs[0].data());
+    console.log(resp.docs[1].data());
+    console.log(resp.docs[2].data());
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 function App() {
   return (
