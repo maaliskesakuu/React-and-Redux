50c326baa580c43d10bcfec2664c5229d70721c4
maaliskesakuu <tanja@prodi.fi>
2020-08-26 09:44:53 +0300

    updated yarn

--- the-social-network/src/index.js
+++ the-social-network/src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { reduxFirestore, getFirestore } from "redux-firestore";
 import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
-import fbConfig from './config/fbConfig'
+import fbConfig from "./config/fbConfig";
 
 const store = createStore(
   rootReducer,
