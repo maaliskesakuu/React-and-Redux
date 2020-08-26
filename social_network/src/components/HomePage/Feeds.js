3c88eb1ae5c9bb779abf39d33aed4da7eaa69831
maaliskesakuu <tanja@prodi.fi>
2020-08-26 18:36:48 +0300

    added HomePage Components

--- social_network/src/components/HomePage/Feeds.js
+++ social_network/src/components/HomePage/Feeds.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import AllPosts from "../Posts/AllPosts";
+import Notifications from "./Notifications";
 
-export default class Feeds extends Component {
+class Feeds extends Component {
   render() {
+    const { posts } = this.props;
     return (
       <div>
-        
+        <div className="container">
+          <div className="row">
+            <div className="col s12 m6">
+              <AllPosts posts={posts} />
+            </div>
+            <div className="col s12 m5 offser-m1">
+              <Notifications />
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
+
+export default Feeds;
--- social_network/src/components/HomePage/Notifications.js
+++ social_network/src/components/HomePage/Notifications.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
-export default function Notifications() {
+const Notifications = () => {
   return (
     <div>
-      
+      <p>Notifications</p>
     </div>
-  )
-}
+  );
+};
+
+export default Notifications;
+
