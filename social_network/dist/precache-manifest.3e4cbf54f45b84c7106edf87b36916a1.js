9f1fa17a8ad1ce3c0b5fbed00ad3f8fb326eb9d8
maaliskesakuu <tanja@prodi.fi>
2020-08-29 14:28:16 +0300

    deployed the app

---
+++ social_network/dist/precache-manifest.3e4cbf54f45b84c7106edf87b36916a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07620110879aa512f2beb4c6e89cac9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "203b922e6fd3efa73abd",
+    "url": "/static/css/main.8957d3c0.chunk.css"
+  },
+  {
+    "revision": "446464f0109ecbdc8f4b",
+    "url": "/static/js/2.f01b868d.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/static/js/2.f01b868d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203b922e6fd3efa73abd",
+    "url": "/static/js/main.71688982.chunk.js"
+  },
+  {
+    "revision": "355b045a765c2e4709b9",
+    "url": "/static/js/runtime-main.0ff819e4.js"
+  }
+]);
